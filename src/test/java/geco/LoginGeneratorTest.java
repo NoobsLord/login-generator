f146fc7dd21091e70213251ed287bc52d1511637
NoobsLord <vincent.vernieres@gmail.com>
2019-11-18 11:27:26 +0100

    fix #3

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,30 @@
+package geco;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginGenerator lg;
+
+    @Before
+    public void setUp() throws Exception {
+        String[] start = new String[]{"JROL", "BPER", "CGUR", "JRAL", "JRAL1"};
+        lg = new LoginGenerator(new LoginService(start));
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom() {
+        String login = lg.generateLoginForNomAndPrenom("Durand", "Paul");
+        Assert.assertEquals("PDUR", login);
+    }
+
+    @Test
+    public void generateLoginForJRAL2(){
+        String login = lg.generateLoginForNomAndPrenom("Ralling", "John");
+        Assert.assertEquals("JRAL2", login);
+    }
+}
